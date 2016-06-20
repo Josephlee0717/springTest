9d79c44c152a9efa892b7fbb0a75434deafab839
cyberwh <josephlee0717@qq.com>
2016-06-20 16:47:35 +0800

    pom update

--- src/main/java/joe/levante/controller/MianController.java
+++ src/main/java/joe/levante/controller/MianController.java
@@ -1,4 +1,12 @@
 package joe.levante.controller;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import net.sf.json.JSONArray;  
+import net.sf.json.JSONObject;  
+import java.util.ArrayList;   
+import java.util.List;  
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,4 +18,22 @@ public class MianController {
         System.out.print("this is a test!");
         return "index";
     }
+	
+	@RequestMapping("/getTestJson")
+	public void getTestJson(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		List m = (List) new  ArrayList();  
+        JSONArray jsons = new JSONArray();  
+//        for(int i=0;i<10;i++){  
+//            User user = new User();  
+//            user.setUserName("name_" + i);  
+//            m.add(user);  
+//        }  
+          
+        for(int j=0;j<m.size();j++){  
+            JSONObject jsonObject = new JSONObject();  
+            jsonObject.put("user", m.get(j));  
+            jsons.add(jsonObject);  
+        }  
+        response.getWriter().print(jsons.toString()) ;   
+	}
 }
